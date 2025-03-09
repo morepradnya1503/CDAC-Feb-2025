b45b43292b6b6b21898b77d68dc21d4a50263607
morepradnya1503 <152419265+morepradnya1503@users.noreply.github.com>
2025-03-09 11:54:07 +0530

    Add files via upload

---
+++ Logic Building Session/Assignment 02_Java Program/AreaSquare.java
@@ -0,0 +1,18 @@
+/*Print Area of a Square: 
+
+   Task: Create a flowchart to calculate and print the area of a square. 
+   Next Step: Write a Java program that calculates the area of a square using the formula 
+              area = side * side. Use a predefined side length.*/
+
+class AreaSquare{
+          
+             public static void main(String args[])
+             {
+              float side = 6.3f; //predifine side
+              float area = side * side;// formula of are of square
+
+         System.out.println("Area of the square with length " + side + " is:" + area);
+}
+             
+
+}

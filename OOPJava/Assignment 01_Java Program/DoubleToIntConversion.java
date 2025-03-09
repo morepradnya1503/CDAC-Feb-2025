c42cd55abc9a49dc39b3c8b8195608960e977254
morepradnya1503 <152419265+morepradnya1503@users.noreply.github.com>
2025-03-09 12:12:47 +0530

    Add files via upload

---
+++ OOPJava/Assignment 01_Java Program/DoubleToIntConversion.java
@@ -0,0 +1,37 @@
+
+
+//Narrowing
+
+
+class DoubleToIntConversion{
+	
+	
+	public static void main(String args[]){
+		double d = 9999.99;
+		int num = (int) d; //Explicit narraowing : double->int
+		
+		System.out.println(d);
+		System.out.println(num);
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ OOPJava/Assignment 01_Java Program/Narrowing.java
@@ -0,0 +1,49 @@
+
+
+//Narrowing
+
+/*Class DoubleToIntConversion{
+	
+	public static void main(String args[]){
+	  double d = 9999.99; 
+	  int num = (int) d ; //Explicit narraowing : double->int
+	
+	  System.out.println(d);
+	  System.out.println(num);
+	}
+}
+
+
+*/
+class Narrowing{
+	
+	
+	public static void main(String args[]){
+		double d = 9999.99;
+		int num = (int) d; //Explicit narraowing : double->int
+		
+		System.out.println(d);
+		System.out.println(num);
+		
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9b825962e2e0ed6b15eb7b2874e6956c36b58bb3
morepradnya1503 <152419265+morepradnya1503@users.noreply.github.com>
2025-03-11 20:30:19 +0530

    Add files via upload

---
+++ OOPJava/Assignment 01/ArithmeticOperators.java
@@ -0,0 +1,30 @@
+//Write a program to demonstrate binary, octal, hexadecimal, and floating-point literals in Java.
+
+/*Write a program that uses boolean literals
+	to control program flow in an if-else statement.*/
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
